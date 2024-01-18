aacf95b53b70e9d9f53dd20ccd79bd317a12291b
smichajlidis <smichajlidis@gmail.com>
2024-01-18 14:05:15 +0100

    define clear() method for each opearating systems

---
+++ src/ConsoleAnimator.cpp
@@ -0,0 +1,19 @@
+#include "../include/ConsoleAnimator.hpp"
+#include <iostream>
+
+ConsoleAnimator::ConsoleAnimator() {}
+
+ConsoleAnimator::~ConsoleAnimator() {}
+
+void ConsoleAnimator::clear() const {
+    #if defined _WIN32
+        system("cls");
+        //clrscr(); // including header file : conio.h
+    #elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+        system("clear");
+        //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+    #elif defined (__APPLE__)
+        system("clear");
+    #endif
+}
+
