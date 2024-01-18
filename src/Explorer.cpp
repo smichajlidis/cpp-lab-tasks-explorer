b887ca746a42c007ecae13e0cf976c76199629ee
smichajlidis <smichajlidis@gmail.com>
2024-01-18 13:11:35 +0100

    add InvalidInputException to validate input

--- src/Explorer.cpp
+++ src/Explorer.cpp
@@ -1,5 +1,7 @@
 #include "../include/Explorer.hpp"
+#include "../include/InvalidInputException.hpp"
 #include <iostream>
+#include <limits>
 
 Explorer::Explorer() {
     mainMenu();
@@ -8,14 +10,28 @@ Explorer::Explorer() {
 Explorer::~Explorer() {}
 
 void Explorer::mainMenu() const {
-    std::cout << "\n\tC++1 LABORATORY TASKS EXPLORER\n" << std::endl;
-    int count = 1;
-    for(const auto& task: tasks) {
-        std::cout << "\t" << count << ".\t" << task->getName() << std::endl;
-        ++count;
-    }
-    std::cout << "\t0.\tExit" << std::endl;
-    int choice; 
-    std::cout << "\n\tWhich task do you want to explore? ";
-    std::cin >> choice;
+    size_t choice;
+    bool invalidInput;
+    do {
+        invalidInput = false;
+        std::cout << "\n\tC++1 LABORATORY TASKS EXPLORER\n" << std::endl;
+        int count = 1;
+        for(const auto& task: tasks) {
+            std::cout << "\t" << count << ".\t" << task->getName() << std::endl;
+            ++count;
+        }
+        std::cout << "\t0.\tExit" << std::endl;
+        std::cout << "\n\tWhich task do you want to explore? ";
+        try {
+            std::cin >> choice;
+            if (std::cin.fail() || choice>tasks.size()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                invalidInput = true;
+                throw InvalidInputException();
+            }
+        } catch (const InvalidInputException &ex) {
+            std::cerr << ex.what() << std::endl;
+        }
+    } while (invalidInput);
 }
