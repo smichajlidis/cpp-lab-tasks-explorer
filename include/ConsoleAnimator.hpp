4cf080f9c404e6db434a730aa970a8bbe5c3b349
smichajlidis <smichajlidis@gmail.com>
2024-01-18 14:04:25 +0100

    declare clear() method

---
+++ include/ConsoleAnimator.hpp
@@ -0,0 +1,12 @@
+#ifndef _CONSOLEANIMATOR_HPP_
+#define _CONSOLEANIMATOR_HPP_
+
+class ConsoleAnimator {
+
+public:
+    void clear() const;
+    ConsoleAnimator();
+    ~ConsoleAnimator();
+};
+
+#endif
