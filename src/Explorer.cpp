5a6f17ebcc320eb3a6cddad894f3a4add316a97a
smichajlidis <smichajlidis@gmail.com>
2024-01-18 12:46:54 +0100

    Add the title and exit option

--- src/Explorer.cpp
+++ src/Explorer.cpp
@@ -1,17 +1,21 @@
 #include "../include/Explorer.hpp"
 #include <iostream>
 
-Explorer::Explorer() {}
+Explorer::Explorer() {
+    mainMenu();
+}
 
 Explorer::~Explorer() {}
 
 void Explorer::mainMenu() const {
+    std::cout << "\n\tC++1 LABORATORY TASKS EXPLORER\n" << std::endl;
     int count = 1;
     for(const auto& task: tasks) {
         std::cout << "\t" << count << ".\t" << task->getName() << std::endl;
         ++count;
     }
-    char choice;
-    std::cout << "\nWhich task do you want to explore?";
+    std::cout << "\t0.\tExit" << std::endl;
+    int choice; 
+    std::cout << "\n\tWhich task do you want to explore? ";
     std::cin >> choice;
 }
