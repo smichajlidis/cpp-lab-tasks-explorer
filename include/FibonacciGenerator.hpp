77b43131009887872b382ca6c318693c4dba8c21
smichajlidis <smichajlidis@gmail.com>
2024-01-19 13:57:29 +0100

    declare fibRecursive method

--- include/FibonacciGenerator.hpp
+++ include/FibonacciGenerator.hpp
@@ -2,12 +2,14 @@
 #define _FIBONACCIGENERATOR_HPP_
 
 #include <string>
+#include <vector>
 #include "Task.hpp"
 
 class FibonacciGenerator: public Task {
 public:
     virtual void execute() const;
     void fibIterative(int) const;
+    std::vector<int> fibRecursive(int) const;
     FibonacciGenerator(const std::string& name);
 };
 
