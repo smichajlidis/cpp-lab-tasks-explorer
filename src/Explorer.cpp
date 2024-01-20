c941ec19bbfab6507933555c579eb8d112db06c6
smichajlidis <smichajlidis@gmail.com>
2024-01-20 15:13:36 +0100

    define taskMenu and integrate it with mainMenu

--- src/Explorer.cpp
+++ src/Explorer.cpp
@@ -16,36 +16,58 @@ Explorer::Explorer() {
 Explorer::~Explorer() {}
 
 void Explorer::mainMenu() const {
-    size_t choice;
-    bool invalidInput;
+    size_t choice = 0;
     do {
-        invalidInput = false;
         std::cout << "\n\tC++1 LABORATORY TASKS EXPLORER\n" << std::endl;
-        int count = 1;
-        for(const auto& task: tasks) {
-            std::cout << "\t" << count << ".\t" << task->getName() << std::endl;
-            ++count;
-        }
-        std::cout << "\t0.\tExit" << std::endl;
-        std::cout << "\n\tWhich task do you want to explore? ";
+
+            int count = 1;
+            for(const auto& task: tasks) {
+                std::cout << "\t" << count << ".\t" << task->getName() << std::endl;
+                ++count;
+            }
+            std::cout << "\t0.\tExit" << std::endl;
+            std::cout << "\n\tWhich task do you want to explore? ";
+            try {
+                std::cin >> choice;
+                if (std::cin.fail() || choice>tasks.size()) {
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    throw InvalidInputException();
+                }
+                else if (choice)
+                    taskMenu(choice);
+            } catch (const InvalidInputException &ex) {
+                consoleAnimator.clear();
+                std::cerr << ex.what() << std::endl;
+            }
+    } while (choice);
+}
+
+void Explorer::taskMenu(int choice) const {
+    
+    consoleAnimator.clear();
+    int subchoice = 1;
+    do {
+        std::cout << "\n\t1.\tRun the program\n";
+        std::cout << "\t2.\tShow me the code!\n";
+        std::cout << "\t0.\tReturn\n";
+
+        std::cout << "\t\nWhat do you want to do? ";
         try {
-            std::cin >> choice;
-            if (std::cin.fail() || choice>tasks.size()) {
+            std::cin >> subchoice;
+            consoleAnimator.clear();
+            if (std::cin.fail() || subchoice>2) {
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                invalidInput = true;
                 throw InvalidInputException();
             }
+            else if (subchoice == 1)
+                tasks.at(choice-1)->execute();
+            else if (subchoice == 2)
+                tasks.at(choice-1)->showCode();
         } catch (const InvalidInputException &ex) {
             consoleAnimator.clear();
             std::cerr << ex.what() << std::endl;
         }
-    } while (invalidInput);
-
-    consoleAnimator.clear();
-    tasks.at(choice-1)->execute();
-
-    std::cout << "What do you want to do now?" << std::endl;
-    std::cin >> choice;
-
+    } while (subchoice);
 }
