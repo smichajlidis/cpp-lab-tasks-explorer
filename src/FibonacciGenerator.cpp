ef77593e6179a70ec1f959a7f3d4c1b6030cc848
smichajlidis <smichajlidis@gmail.com>
2024-01-23 17:39:49 +0100

    change the order of the methods to display the code in the right way

--- src/FibonacciGenerator.cpp
+++ src/FibonacciGenerator.cpp
@@ -4,6 +4,23 @@
 FibonacciGenerator::FibonacciGenerator(const std::string& name)
     : Task(name) {}
 
+std::vector<int> FibonacciGenerator::fibRecursive(int n) const {
+    std::vector<int> result;
+    if (n < 1) {
+        return result;
+    }
+    result.push_back(0);
+    if (n == 1) {
+        return result;
+    }
+    result.push_back(1);
+    for (int i = 2; i < n; ++i) {
+        int nextNumber = result[i - 1] + result[i - 2];
+        result.push_back(nextNumber);
+    }
+    return result;
+}
+
 void FibonacciGenerator::execute() const {
     int n;
     std::cout << "How many Fibonacci numbers do you want to generate?" << std::endl;
@@ -38,23 +55,6 @@ void FibonacciGenerator::fibIterative(int n) const {
     }
 }
 
-std::vector<int> FibonacciGenerator::fibRecursive(int n) const {
-    std::vector<int> result;
-    if (n < 1) {
-        return result;
-    }
-    result.push_back(0);
-    if (n == 1) {
-        return result;
-    }
-    result.push_back(1);
-    for (int i = 2; i < n; ++i) {
-        int nextNumber = result[i - 1] + result[i - 2];
-        result.push_back(nextNumber);
-    }
-    return result;
-}
-
 #include <fstream>
 
 void FibonacciGenerator::showCode() const {
@@ -62,29 +62,26 @@ void FibonacciGenerator::showCode() const {
     std::ifstream file {"../src/FibonacciGenerator.cpp"};
 
     if (file) {
-
-        std::cout << "#include <iostream>\n\nint main() {" << std::endl;
-
+        std::cout << "#include <iostream>" << std::endl;
         std::string line;
         int count = 1;
         while (std::getline(file, line)) {
-            if (count>=8 && count<=10)
-                std::cout << line << std::endl;
-            else if (count>=16 && count<=18 && this->getName()=="Fibonacci Generator Recursive")
-                std::cout << line << std::endl;     
-            else if (count>=26 && count<=38 && this->getName()=="Fibonacci Generator Iterative") {
-                if (count == 26) std::cout << "\n";
+            if (count>=8 && count<=22 && this->getName()=="Fibonacci Generator Recursive") {
+                if (count == 8) std::cout << "#include <vector>\n\nstd::vector<int> fibRecursive(int n) {\n";
                 std::cout << line << std::endl;
             }
-            else if (count>=42 && count<=55 && this->getName()=="Fibonacci Generator Recursive") {
-                if (count == 42) std::cout << "}\n\nstd::vector<int> fibRecursive(int n) {" << std::endl;
+            else if (count>=25 && count<=27) {
+                if (count == 25) std::cout << "\nint main() {" << std::endl;
+                std::cout << line << std::endl;
+            }
+            else if (count>=33 && count<=36 && this->getName()=="Fibonacci Generator Recursive") {
+                std::cout << line << std::endl;
+            }
+            else if (count>=43 && count<=56 && this->getName()=="Fibonacci Generator Iterative") {
                 std::cout << line << std::endl;
             }
             ++count;
         }
-        std::cout << "}" << std::endl;
-
-        
     }
     else
         std::cout << "Error loading file" << std::endl;
