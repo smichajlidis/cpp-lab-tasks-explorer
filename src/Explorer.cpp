91127bf8cfc722b8150c7255aa7a698fbc6aef9b
smichajlidis <smichajlidis@gmail.com>
2024-01-23 17:58:26 +0100

    define printHeader() method and use it in taskMenu and mainMenu methods

--- src/Explorer.cpp
+++ src/Explorer.cpp
@@ -21,28 +21,27 @@ Explorer::~Explorer() {}
 void Explorer::mainMenu() const {
     size_t choice = 0;
     do {
-        std::cout << "\n\tC++1 LABORATORY TASKS EXPLORER\n" << std::endl;
-
-            int count = 1;
-            for(const auto& task: tasks) {
-                std::cout << "\t" << count << ".\t" << task->getName() << std::endl;
-                ++count;
+        printHeader();
+        int count = 1;
+        for(const auto& task: tasks) {
+            std::cout << "\t" << count << ".\t" << task->getName() << std::endl;
+            ++count;
+        }
+        std::cout << "\t0.\tExit" << std::endl;
+        std::cout << "\n\tWhich task do you want to explore? ";
+        try {
+            std::cin >> choice;
+            if (std::cin.fail() || choice>tasks.size()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                throw InvalidInputException();
             }
-            std::cout << "\t0.\tExit" << std::endl;
-            std::cout << "\n\tWhich task do you want to explore? ";
-            try {
-                std::cin >> choice;
-                if (std::cin.fail() || choice>tasks.size()) {
-                    std::cin.clear();
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                    throw InvalidInputException();
-                }
-                else if (choice)
-                    taskMenu(choice);
-            } catch (const InvalidInputException &ex) {
-                consoleAnimator.clear();
-                std::cerr << ex.what() << std::endl;
-            }
+            else if (choice)
+                taskMenu(choice);
+        } catch (const InvalidInputException &ex) {
+            consoleAnimator.clear();
+            std::cerr << ex.what() << std::endl;
+        }
     } while (choice);
 }
 
@@ -51,11 +50,12 @@ void Explorer::taskMenu(int choice) const {
     consoleAnimator.clear();
     int subchoice = 1;
     do {
-        std::cout << "\n\t1.\tRun the program\n";
+        printHeader();
+        std::cout << "\t1.\tRun the program\n";
         std::cout << "\t2.\tShow me the code!\n";
         std::cout << "\t0.\tReturn\n";
 
-        std::cout << "\t\nWhat do you want to do? ";
+        std::cout << "\n\tWhat do you want to do? ";
         try {
             std::cin >> subchoice;
             consoleAnimator.clear();
@@ -73,4 +73,10 @@ void Explorer::taskMenu(int choice) const {
             std::cerr << ex.what() << std::endl;
         }
     } while (subchoice);
+}
+
+void Explorer::printHeader() const {
+    std::cout << "\t..........................................\n";
+    std::cout << "\t----- C++1 LABORATORY TASKS EXPLORER -----\n";
+    std::cout << "\t``````````````````````````````````````````" << std::endl;
 }
